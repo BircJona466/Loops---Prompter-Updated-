146ee6c508e06f469867bb796b8d460420188eaa
Jonah Birch <BircJona466@GoTVDSB.ca>
2021-10-06 13:58:56 -0400

    Add project files.

---
+++ Part 5 - Loops/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Part_5___Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min;
+            int max;
+            int i;
+            
+
+            Console.WriteLine("Please enter your minimum number..");
+            min = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter your maximum number..");
+            max = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a number between " + min + " and " + max );
+            i = Convert.ToInt32(Console.ReadLine());
+
+            while ((i <= min || (i >= max)))
+            { 
+
+                Console.WriteLine("That's not a number between " + min + " and " + max );
+                    Console.WriteLine("Please enter a number between " + min + " and " + max );
+                    i = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Thank you!");
+
+
+
+
+
+
+
+
+            }
+    }
+}
